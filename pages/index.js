efefe344a8e3aa0b75ce68b19d9ca582f9a8438d
Baraa Baba <99536124+Baraa-Baba@users.noreply.github.com>
2022-03-15 15:09:22 +0200

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,12 @@ import { useEffect } from 'react';
 import dynamic from 'next/dynamic'
 const Introdution = dynamic(() => import('../components/Introdution'))
 const About = dynamic(() => import('../components/About.jsx'))
-const Projects = dynamic(() => import('../components/projects.jsx'))
 export default function App() {
   useEffect(() => {
     window.scrollTo(0, document.body.clientHeight)
   }, []);
   return <div className="space pb-10">
     <Jet />
-    <Projects />
     <About />
     <Introdution />
   </div>;
