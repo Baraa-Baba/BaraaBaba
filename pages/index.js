4b0ca02f8174d56cbd40d6e1c77aa3aac327e8ca
Baraa Baba <99536124+Baraa-Baba@users.noreply.github.com>
2022-03-17 19:15:51 +0200

    fixed deployment issue

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,16 @@ import Jet from '../components/jet.jsx'
 import { useEffect } from 'react';
 import dynamic from 'next/dynamic'
 const Introdution = dynamic(() => import('../components/Introdution'))
+const Projects = dynamic(() => import('../components/Projects.jsx'))
+const About = dynamic(() => import('../components/About.jsx'))
 export default function App() {
   useEffect(() => {
     window.scrollTo(0, document.body.clientHeight)
   }, []);
   return <div className="space pb-10">
     <Jet />
+    <Projects />
+    <About />
     <Introdution />
   </div>;
 }
