41b4425b4b5e2fc72404a4e7b87c64fc5ba7bbdd
Baraa Baba <99536124+Baraa-Baba@users.noreply.github.com>
2022-03-18 17:46:43 +0200

    speeded up the FCP

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import Jet from '../components/jet.jsx'
 import { useEffect } from 'react';
 import dynamic from 'next/dynamic'
+const Jet = dynamic(() => import('../components/jet'))
 const Introdution = dynamic(() => import('../components/Introdution'))
-const Projects = dynamic(() => import('../components/projects.jsx'))
 const About = dynamic(() => import('../components/About.jsx'))
+const Projects = dynamic(() => import('../components/projects.jsx'))
 export default function App() {
   useEffect(() => {
     window.scrollTo(0, document.body.clientHeight)
