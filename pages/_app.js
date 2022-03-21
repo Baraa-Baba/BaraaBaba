eccc0d6cf5d1a2013cf57b2e62cf9ba7ef1debed
Baraa Baba <99536124+Baraa-Baba@users.noreply.github.com>
2022-03-21 21:49:41 +0200

    added a basic loading screen

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,13 @@ import '../styles/globals.css'
 import { useEffect, useState } from 'react'
 function MyApp({ Component, pageProps }) {
   const [loading, setLoading] = useState(true);
-
   useEffect(() => {
     setLoading(false);
     window.scrollTo(0, document.body.clientHeight)
   }, []);
   return (
     <>
-      {!loading ? <Component {...pageProps} /> : <p>'loading...'</p>}
+      {!loading ? <Component {...pageProps} /> : <p>loading...</p>}
     </>
   )
 }
