9d6ed1078772846d11ab1c0d371750e819527313
Baraa Baba <99536124+Baraa-Baba@users.noreply.github.com>
2022-04-08 17:18:55 +0300

    Add files via upload

--- components/About.jsx
+++ components/About.jsx
@@ -2,7 +2,8 @@ import Card from "./Card"
 const About = () => {
   return (
     <div className="max-w-[100vw] mb-10" ref={About}>
-      <Card index={4} img={'md:inline-block'} links="none" header={'Baraa'} text={"Iam Baraa a 15 years old from Lebanon. I love programming and I am a self taught frontend developer."} />
+      <Card index={4} img={'md:inline-block'} links="none" header={'Baraa'}
+        text={"Hi my name is baraa a teen from Lebanon I love programming and I am a self taught frontend developer."} />
       <Card index={5} img={'md:inline-block'} links="GTD" header={'contributor'} text={'I love contributing in open source projects in github and I am active in twitter and dev.to.'} />
       <Card index={9} img={"md:inline-block"} links="FM" header={'freelancer'}
         text={" I am a freelancer at freelancer.com and mostqel this really helps building my experience."} />
