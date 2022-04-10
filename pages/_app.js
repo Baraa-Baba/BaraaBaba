4ea7c82ff7404ccb81a32fd5a8c4a18134b74051
Baraa Baba <99536124+Baraa-Baba@users.noreply.github.com>
2022-04-10 18:53:18 +0300

    google vrefication

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import { useEffect, useState } from 'react'
 import LoadingPage from '../components/LoadingPage';
+import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
@@ -8,6 +9,15 @@ function MyApp({ Component, pageProps }) {
   }, [])
   return (
     <>
+      <Head>
+        <title>Baraa Baba</title>
+        <meta name="google-site-verification" content="Pp6HCNnRnfSMweHewGJ8XIWEwCJ_cGOiDa2aOQAKC7w" />
+        <meta author='baraa baba'></meta>
+        <meta name="Description" content="Hi my name is Baraa a frontend developer from lebeneon I love
+      programming and learning new things all the time and this is my portfilio website" />
+        <meta name="description" content="Hi my name is Baraa a frontend developer from lebeneon I love
+    programming and learning new things all the time and this is my portfilio website" />
+      </Head>
       {!loading ? <Component loading={loading} {...pageProps} /> : <LoadingPage />}
     </>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import { useEffect } from 'react';
 import dynamic from 'next/dynamic'
-import head from 'next/head'
 import Introdution from '../components/Introdution'
 const Jet = dynamic(() => import('../components/jet'))
 const About = dynamic(() => import('../components/About.jsx'))
@@ -18,9 +17,10 @@ export default function App({ loading }) {
   }, [loading])
   return <div className="space p-0 m-0">
     <head>
+      <meta name="google-site-verification" content="Pp6HCNnRnfSMweHewGJ8XIWEwCJ_cGOiDa2aOQAKC7w" />
       <title>Baraa Baba</title>
       <meta author='baraa baba'></meta>
-      <meta name="description" content="Hi my name is Baraa a frontend developer from lebeneon I love
+      <meta name="Description" content="Hi my name is Baraa a frontend developer from lebeneon I love
       programming and learning new things all the time and this is my portfilio website" />
     </head>
     <Jet />
