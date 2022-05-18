142b70a669be5cb28963965a8a351e970c52157b
Baraa Baba <99536124+Baraa-Baba@users.noreply.github.com>
2022-05-18 18:30:25 +0300

    made slider more responsive

--- components/projects.jsx
+++ components/projects.jsx
@@ -1,26 +1,37 @@
 import Image from 'next/image'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import hangmanss from '../assets/hangman.png'
 import todolist from '../assets/todolist.png'
 import imageeditorimg from '../assets/imageeditor.png'
+
+import Pokemonimg from '../assets/poke-pagnation.png'
+import yousafzaiimg from '../assets/malalaimg.png'
 import { Carousel } from 'react-responsive-carousel';
 const Projects = () => {
     const [hangman_desc, sethangman_desc] = useState(false)
     const [todo_desc, settodo_desc] = useState(false)
     const [imageeditor, setimageeditor] = useState(false)
+    const [Pokemon, setPokemon] = useState(false);
+    const [yousafzai, setyousafzai] = useState(false);
+    const [width, setwidth] = useState(0);
+    useEffect(() => {
+        setwidth(innerWidth)
+        window.addEventListener('resize', (() => {
+            setwidth(innerWidth)
+        }))
+    }, [])
     return (
         <div id='projects' className="carousel-wrapper text-white z-10" >
             <Carousel infiniteLoop dynamicHeight={false} >
-                <div id='hangman-game' tabIndex={14} className='relative h-[100vh] w-[100vw]'>
+                <div id='hangman-game' tabIndex={14} className='project-cont'>
                     <a href="https://baraa-baba.github.io/hangman-game/"
                         target={'about:blank'} className="text-white text-4xl">hangman game</a>
                     <button title='click to see description' onClick={() => sethangman_desc(!hangman_desc)}
                         className="text-4xl cursor-pointer rounded-full bg-green-400 ml-5">?</button>
                     {/*this is div is to contain the iframe and overlay so the overlay fits perfectly*/}
-                    <div className='relative'>
-                        <iframe className="relative h-[100vh] w-[100vw]"
+                    <div className='project-iframe-cont'>
+                        <iframe id='margin_000' className="project-iframe"
                             src="https://baraa-baba.github.io/hangman-game/">
-                            <Image src={hangmanss} width={999} height={999} alt='hangman website screenshot' />
                         </iframe>
                         {hangman_desc && <div className='project-desc-cont'>
                             <h3 className='text-white text-5xl'>Description:</h3>
@@ -39,17 +50,15 @@ const Projects = () => {
                         </div>}
                     </div>
                 </div>
-                <div id='to-do-list' tabIndex={20} className="relative h-[100vh] w-[100vw]">
+                <div id='to-do-list' tabIndex={20} className="project-cont">
                     <a href="https://baraa-baba.github.io/to-do-list/" target={'about:blank'}
                         className="text-white text-4xl">To do list</a>
                     <button title='click to see description' onClick={() => settodo_desc(!todo_desc)}
                         className="text-4xl bg-green-400 ml-5">?</button>
-                    <div className='relative'>
-                        <iframe className="relative h-[100vh] w-[100vw]"
+                    <div className='project-iframe-cont'>
+                        <iframe id='margin_00' className="project-iframe"
                             src="https://baraa-baba.github.io/to-do-list/">
-                            <Image src={todolist} width={999} height={999} alt='hangman website screenshot' />
                         </iframe>
-
                         {todo_desc && <div className='project-desc-cont'>
                             <h3 className='text-white text-5xl'>Description:</h3>
                             <p className='project-desc-text'>
@@ -69,16 +78,15 @@ const Projects = () => {
                     </div>
 
                 </div>
-                <div id='image-editor' tabIndex={30} className='relative h-[100vh] w-[100vw]'>
+                <div id='image-editor' tabIndex={30} className='project-cont'>
                     <a href="https://baraa-baba.github.io/image-editor/" target={'about:blank'} className="text-white text-4xl">image editor</a>
 
                     <button title='click to see description'
                         onClick={() => setimageeditor(!imageeditor)}
                         className="hover-appear text-4xl bg-green-400 ml-5">?</button>
-                    <div className='relative h-[100vh] w-[100vw]'>
-                        <iframe className="relative h-[100vh] w-[100vw]"
+                    <div className='project-iframe-cont'>
+                        <iframe id='margin_0' className="project-iframe"
                             src="https://baraa-baba.github.io/image-editor/">
-                            <Image src={imageeditorimg} width={999} height={999} alt='hangman website screenshot' />
                         </iframe>
                         {imageeditor &&
                             <div className='project-desc-cont'>
@@ -98,20 +106,20 @@ const Projects = () => {
                             </div>}
                     </div>
                 </div>
-                <div id=' react Pokémon pagination' tabIndex={30} className='relative h-[100vh] w-[100vw]'>
+                <div id=' react Pokémon pagination' tabIndex={30} className='project-cont'>
                     <a href="https://baraa-baba.github.io/React-Pokemon-Pagination/"
                         target={'about:blank'} className="text-white text-4xl">
                         react Pokémon pagination </a>
 
                     <button title='click to see description'
-                        onClick={() => setimageeditor(!imageeditor)}
+                        onClick={() => setPokemon(!Pokemon)}
                         className="hover-appear text-4xl bg-green-400 ml-5">?</button>
-                    <div className='relative h-[100vh] w-[100vw]'>
-                        <iframe className="relative h-[100vh] w-[100vw]"
+                    <div className='project-iframe-cont'>
+                        <iframe id='margin_00000' className="project-iframe"
                             src="https://baraa-baba.github.io/React-Pokemon-Pagination/">
-                            <Image src={imageeditorimg} width={999} height={999} alt='hangman website screenshot' />
+                            <Image src={Pokemonimg} alt='hangman website screenshot' />
                         </iframe>
-                        {imageeditor &&
+                        {Pokemon &&
                             <div className='project-desc-cont '>
                                 <h3 className='text-white text-5xl'>Description:</h3>
                                 <p className='project-desc-text'>
@@ -131,20 +139,20 @@ const Projects = () => {
                             </div>}
                     </div>
                 </div>
-                <div id='Malala yousafzai website' tabIndex={30} className='relative h-[100vh] w-[100vw]'>
+                <div id='Malala yousafzai website' tabIndex={30} className='project-cont'>
                     <a href="https://baraa-baba.github.io/Malala-Yousafzai/"
                         target={'about:blank'} className="text-white text-4xl">
                         Malala yousafzai website </a>
 
                     <button title='click to see description'
-                        onClick={() => setimageeditor(!imageeditor)}
+                        onClick={() => setyousafzai(!yousafzai)}
                         className="hover-appear text-4xl bg-green-400 ml-5">?</button>
-                    <div className='relative h-[100vh] w-[100vw]'>
-                        <iframe className="relative h-[100vh] w-[100vw]"
+                    <div className='project-iframe-cont'>
+                        <iframe id='margin_0000' className="project-iframe"
                             src="https://baraa-baba.github.io/Malala-Yousafzai/">
-                            <Image src={imageeditorimg} width={999} height={999} alt='hangman website screenshot' />
+                            <Image src={yousafzaiimg} width={999} height={999} alt='hangman website screenshot' />
                         </iframe>
-                        {imageeditor &&
+                        {yousafzai &&
                             <div className='project-desc-cont '>
                                 <h3 className='text-white text-5xl'>Description:</h3>
                                 <p className='project-desc-text'>
