1763748adddc42b2794e4873e28d5d9d99125902
Baraa Baba <99536124+Baraa-Baba@users.noreply.github.com>
2023-09-24 17:22:01 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -11,8 +11,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Baraa Baba</title>
-        <meta name="google-site-verification" content="Pp6HCNnRnfSMweHewGJ8XIWEwCJ_cGOiDa2aOQAKC7w" />
+        <title>Baraa Baba</title> 
         <meta author='baraa baba'></meta>
         <meta name="Description" content="Hi my name is Baraa a frontend developer from lebeneon I love
       programming and learning new things all the time and this is my portfilio website" />
